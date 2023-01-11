1acdd8ec6769f7ad4dc38341a92a08f845758fb8
yandex-lyceum-yakovlev <yakovlev@yandexlyceum.ru>
2023-01-11 18:05:02 +0400

    grid

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import tkinter
 
+
+w = tkinter.Tk()
+c = tkinter.Canvas(width=500, height=500, background="white")
+c.pack()
+n, m = 10, 10
+cw, ch = 500 // n, 500 // m
+for i in range(n):
+    for j in range(m):
+        c.create_rectangle((i * cw + 2, j * ch + 2), ((i + 1) * cw, (j + 1) * ch), outline="black")
+w.mainloop()
