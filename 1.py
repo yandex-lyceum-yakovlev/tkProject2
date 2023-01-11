0e6c9942ea2cefd4dce9f867fd2e47f20814a245
yandex-lyceum-yakovlev <yakovlev@yandexlyceum.ru>
2023-01-11 18:41:50 +0400

    KeyPress

--- 1.py
+++ 1.py
@@ -2,11 +2,23 @@ import tkinter
 
 
 def f(event):
-    c.create_oval((event.x - 10,event.y - 10), (event.x + 10,event.y + 10), fill="red")
+    c.create_oval((event.x - 10,event.y - 10), (event.x + 10,event.y + 10), fill=color)
+
+
+def keypress(event):
+    global color
+    if event.keysym == "r":
+        color = "red"
+    if event.keysym == "g":
+        color = "green"
+    if event.keysym == "b":
+        color = "blue"
 
 
 w = tkinter.Tk()
+color = "red"
 c = tkinter.Canvas(width=500, height=500, background="white")
 c.pack()
 c.bind("<Motion>", f)
+w.bind("<KeyPress>", keypress)
 w.mainloop()
